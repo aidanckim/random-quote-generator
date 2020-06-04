52b1c9cebdc9e7418b4e1f5183fb599103ecb48f
Christopher Aidan Kim <christopheraidank@gmail.com>
2020-06-03 23:05:20 -0700

    Finished random quote generator
    Randomly select a quote once button is clicked

--- js/script.js
+++ js/script.js
@@ -1,33 +1,70 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+/****************
+ARRAYS OF OBJECTS
+*****************/ 
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+const quotes = [
+  { 
+    quote: "You Miss 100% Of The Shots You Don't Take.",
+    source: "Wayne Gretzky",
+    citation: "The Hockey News",
+    year: 1983,
+    tags: "Hockey Hall of Famer"
+  },
+  { 
+    quote: "I think it is possible for ordinary people to choose to be extraordinary.",
+    source: "Elon Musk",
+    citation: "Twitter",
+    year: 2017,
+    tags: "Entrepreneur"
+  },
+  { 
+    quote: "The cynics may be the loudest voices - but I promise you, they will accomplish the least.",
+    source: "Barack Obama",
+    citation: "Business Insider",
+    year: 2013,
+    tags: "Former President of the United States of America"
+  },
+  { 
+    quote: "When you undervalue what you do, the world will undervalue who you are.",
+    source: "Oprah Winfrey",
+    tags: "Philanthropist"
+  },
+  { 
+    quote: "Your time is limited, so don’t waste it living someone else’s life.",
+    source: "Steve Jobs",
+    tags: "Business magnate"
+  },
+];
 
-/*** 
- * `quotes` array 
-***/
+/***************
+GET RANDOM QUOTE
+****************/ 
 
+function getRandomQuote() {
+  return quotes[(Math.floor(Math.random() * quotes.length))];
+}
 
+/**********
+PRINT QUOTE
+***********/ 
 
-/***
- * `getRandomQuote` function
-***/
+function printQuote() {
+  const randomQuote = getRandomQuote();
+  let html = `<p class="quote">${randomQuote.quote}</p>
+              <p class="source">${randomQuote.source}`;
+  if (randomQuote.citation) { 
+    html += `<span class="citation">${randomQuote.citation}</span>`
+  }
+  if (randomQuote.year) {
+    html += `<span class="year">${randomQuote.year}</span>`
+  }
+  html += `</p>`;
+  document.getElementById('quote-box').innerHTML = html;
+  return html;
+}
 
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+/***********
+EVENT LISTENER
+************/ 
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
